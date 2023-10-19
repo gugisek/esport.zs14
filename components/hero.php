8fca41504f6b3a3be8b78f2bf5f91de057905f53
gugisek <gugisek@gmail.com>
2023-10-19 17:54:08 +0200

    dalej nie umiem

--- components/hero.php
+++ components/hero.php
@@ -12,8 +12,9 @@
         </nav>
         <section class="py-32">
             <h1 class="font-bold text-4xl text-white uppercase w-1/2">Witamy was w strefie eventów<br> w <span class="text-green-500">zespole szkół nr. 14</span></h1>
-            <p class="w-2/5 text-gray-300 text-md font-medium">Znajdziesz tutaj wszytko o nadchodzących wydarzeniach dla naszych uczniów!</p>
-            <button></button>
+            <p class="py-2 w-2/5 text-gray-300 text-md font-medium">Znajdziesz tutaj wszytko o nadchodzących wydarzeniach dla naszych uczniów!</p>
+            <button class="border-2 mr-3 border-green-500 uppercase rounded-xl py-2 px-4 text-white hover:scale-105 transition-all duration-300 font-medium">Stream</button>
+            <button class="bg-green-500 rounded-xl py-2 px-4 text-white uppercase font-medium shadow-md hover:scale-105 transition-all duration-300 shadow-green-500">EVENTY</button>
         </section>
     </section>
 </section>
