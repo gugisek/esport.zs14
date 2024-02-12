407e0431e2e1a9daf2ff3b199abf347ccc4d177a
gugisek <gugisek@gmail.com>
2024-02-12 14:17:07 +0100

    fix

--- components/panel/teams.php
+++ components/panel/teams.php
@@ -265,19 +265,19 @@ include "../../scripts/security.php";
         </div>
         
         <dl class="mt-6 divide-y divide-gray-200">
-        <div data-aos="fade-up" data-aos-delay="'.$i.'00" data-aos-anchor-placement="bottom-bottom" class="">
+        <div data-aos="fade-up" data-aos-delay="'100" data-aos-anchor-placement="top-bottom" class="">
           <dt class="text-lg">
             <!-- Expand/collapse question button -->
-            <button type="button" class="pt-5 pb-3 flex w-full items-start justify-between text-left text-gray-400" aria-controls="faq-'.$row['id'].'" aria-expanded="false">
+            <button onclick="expandTeamsToggle(`txt1`)" type="button" class="pt-5 pb-3 flex w-full items-start justify-between text-left text-gray-400" aria-controls="event_txt1" aria-expanded="false">
               <span class="font-medium text-sm text-gray-300">Odpowiedź na zgłoszenie</span>
               <span class="ml-6 flex h-7 items-center">
-                <svg class="rotate-0 duration-300 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <svg id="svg_event_txt1" class="rotate-0 duration-300 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
               </span>
             </button>
           </dt>
-          <dd style="scale: 0; height: 0;" class="duration-300 mt-2 pr-12" id="faq-'.$row['id'].'">
+          <dd style="scale: 0; height: 0;" class="duration-300 mt-2 pr-12" id="event_txt1">
             <div class="text-xs text-gray-400 pb-4">
               Temat: Potwierdzenie zgłoszenia drużyny do turnieju i ważne informacje
                   <br/><br/>
@@ -324,43 +324,6 @@ include "../../scripts/security.php";
       </div>
     </div>
   </section>
-
-<script>
-  // Pobierz wszystkie przyciski "Expand/collapse question"
-  const questionButtons = document.querySelectorAll('button[aria-controls]');
-
-  // Dodaj obsługę kliknięcia dla każdego przycisku
-  questionButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const targetId = button.getAttribute('aria-controls');
-      const target = document.getElementById(targetId);
-
-      if (target) {
-        const expanded = button.getAttribute('aria-expanded') === 'true';
-
-        // Zmień stan aria-expanded na przeciwny (true na false, false na true)
-        button.setAttribute('aria-expanded', !expanded);
-
-        // Zmień ikonę rozwijania/zwijania
-        const icon = button.querySelector('svg');
-        if (icon) {
-          icon.classList.toggle('rotate-0', expanded);
-          icon.classList.toggle('-rotate-180', !expanded);
-        }
-
-        // Pokaż lub ukryj odpowiedź na pytanie
-        if (expanded) {
-          target.style.scale = '0';
-          target.style.height = '0';
-        } else {
-          target.style.scale = '1';
-            target.style.height = 'auto';
-        }
-      }
-    });
-  });
-</script>
-
 
   <script>
     function openSecFilterOpen(id) {
