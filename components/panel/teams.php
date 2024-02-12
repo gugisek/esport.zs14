77b1198bc9945fb779aaa42ba76de89cf1f5ea5f
gugisek <gugisek@gmail.com>
2024-02-12 13:52:01 +0100

    add sample wiadomosci in panel

--- components/panel/teams.php
+++ components/panel/teams.php
@@ -254,6 +254,59 @@ include "../../scripts/security.php";
       ?>
     </ol>
   </section>
+<hr class="border-white/5">
+<div data-aos="fade-right" data-aos-delay="100" class="divide-y divide-white/5" enctype="multipart/form-data">
+    <div class="sm:px-6 lg:px-8 px-4">
+        <div class="px-4 mb-6 sm:px-0 mt-6 flex flex-row justify-between items-center">
+            <div>
+                <h3 class="text-base font-semibold leading-7 text-white">Przykładowe wiadomości</h3>
+                <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-400">Samplowe wiadomości do odpisywania na zgłoszenia.</p>
+            </div>
+        </div>
+        
+        <dl class="mt-6 divide-y divide-gray-200">
+        <div data-aos="fade-up" data-aos-delay="'.$i.'00" data-aos-anchor-placement="bottom-bottom" class="">
+          <dt class="text-lg">
+            <!-- Expand/collapse question button -->
+            <button type="button" class="pt-5 pb-3 flex w-full items-start justify-between text-left text-gray-400" aria-controls="faq-'.$row['id'].'" aria-expanded="false">
+              <span class="font-medium text-sm text-gray-300">Odpowiedź na zgłoszenie</span>
+              <span class="ml-6 flex h-7 items-center">
+                <svg class="rotate-0 duration-300 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+              </span>
+            </button>
+          </dt>
+          <dd style="scale: 0; height: 0;" class="duration-300 mt-2 pr-12" id="faq-'.$row['id'].'">
+            <div class="text-xs text-gray-400 pb-4">
+              Temat: Potwierdzenie zgłoszenia drużyny do turnieju i ważne informacje
+                  <br/><br/>
+              Szanowni uczestnicy,
+                  <br/>
+              Dziękujemy za zgłoszenie drużyny G4rnuchy na nadchodzący turniej "Turniej o Mistrzostwo Szanujący CS:2 2024". Cieszymy się, że dołączyliście do naszej imprezy i mamy nadzieję, że będzie to dla Was ekscytujące doświadczenie!
+                  <br/><br/>
+              Jest nam miło poinformować, że zgłoszenie drużyny zostało przyjęte. Poniżej znajdują się informacje dotyczące dalszych kroków oraz kilka ważnych rzeczy, na które chcielibyśmy zwrócić Waszą uwagę:
+                  <br/><br/>
+              Dołącz do serwera Discord: Zachęcamy wszystkich uczestników do dołączenia do naszego serwera Discord, gdzie będziemy udostępniać najnowsze informacje dotyczące Turnieju, ogłaszać harmonogramy i umożliwiać komunikację między uczestnikami oraz organizatorami. Link do serwera Discord.
+                  <br/><br/>
+              Zapoznanie się z regulaminem: Przeczytajcie uważnie regulamin Turnieju, abyście byli dobrze przygotowani na wydarzenie. Regulamin można znaleźć na naszej stronie internetowej pod adresem [link_do_regulaminu].
+                  <br/><br/>
+              Prosimy o potwierdzenie otrzymania tej wiadomości oraz zapewnienie, że każdy z uczestników zapoznał się z nią oraz z regulaminem Turnieju.
+                  <br/><br/>
+              Jeśli macie jakiekolwiek pytania lub wątpliwości, nie wahajcie się skontaktować z nami poprzez serwer Discord lub odpowiedzcie na tę wiadomość.
+                  <br/><br/>
+              Życzymy powodzenia i niech najlepszy zespół zwycięży!
+                  <br/><br/>
+              Z poważaniem,<br/>
+              Zespół Organizacyjny "Turniej o Mistrzostwo Szanujący CS:2 2024"
+            </div>
+          </dd>
+        </div>
+        </div>
+    </dl>
+    </div>
+</div>
+
 <section id="popupTeamsBg" class="fixed z-[50] h-0 opacity-0 top-0 left-0 w-full h-full bg-[#0000009e] transition-opacity duration-300"></section>
   <section id="popupTeams" onclick="popupTeamsCloseConfirm()" class="z-[60] fixed scale-0 top-0 left-0 w-full h-full">
     <div class="flex items-center justify-center w-full h-full px-2">
@@ -273,6 +326,43 @@ include "../../scripts/security.php";
       </div>
     </div>
   </section>
+
+<script>
+  // Pobierz wszystkie przyciski "Expand/collapse question"
+  const questionButtons = document.querySelectorAll('button[aria-controls]');
+
+  // Dodaj obsługę kliknięcia dla każdego przycisku
+  questionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const targetId = button.getAttribute('aria-controls');
+      const target = document.getElementById(targetId);
+
+      if (target) {
+        const expanded = button.getAttribute('aria-expanded') === 'true';
+
+        // Zmień stan aria-expanded na przeciwny (true na false, false na true)
+        button.setAttribute('aria-expanded', !expanded);
+
+        // Zmień ikonę rozwijania/zwijania
+        const icon = button.querySelector('svg');
+        if (icon) {
+          icon.classList.toggle('rotate-0', expanded);
+          icon.classList.toggle('-rotate-180', !expanded);
+        }
+
+        // Pokaż lub ukryj odpowiedź na pytanie
+        if (expanded) {
+          target.style.scale = '0';
+          target.style.height = '0';
+        } else {
+          target.style.scale = '1';
+            target.style.height = 'auto';
+        }
+      }
+    });
+  });
+</script>
+
 
   <script>
     function openSecFilterOpen(id) {
